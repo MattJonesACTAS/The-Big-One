a4a50ec26e61074cb3ecb37eb656b1e79067afcc
MattJonesACTAS <infomattjns@gmail.com>
2026-07-18 01:50:30 +1000

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -224,7 +224,10 @@ const formatTimeWithSeconds = (seconds: number) => {
 const formatTimeHMM = (seconds: number) => {
   const hours = Math.floor(seconds / 3600);
   const mins = Math.floor((seconds % 3600) / 60);
-  return hours > 0 ? `${hours}h, ${mins}m` : `${mins}m`;
+  const secs = seconds % 60;
+  if (hours > 0) return `${hours}h, ${mins}m, ${secs}s`;
+  if (mins > 0) return `${mins}m, ${secs}s`;
+  return `${secs}s`;
 };
 
 const getLocalTime = (date?: Date) => {
